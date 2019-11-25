ff04ad1166340fb0f77398365b6055e9e8095f24
Ngoc Son <son.95.1112@gmail.com>
2019-11-25 13:24:40 +0700

    Add feature paging Food list

--- src/milktea-project/components/order/product-list.tsx
+++ src/milktea-project/components/order/product-list.tsx
@@ -5,25 +5,29 @@ import { Food } from '../../model/Food';
 import { compose } from 'redux';
 import { AdditionalForm } from './modal/additional-food';
 import { Modal } from '../../../common/components/modal';
-import {GetFoodListAction} from "../../redux/action/actions";
+import {GetFoodListAction, SetPageListFood} from "../../redux/action/actions";
 
 interface StateToProps {
     food?: Food[];
 }
 interface DispatchToProps {
     getFoodList: () => void;
+    setPageListFood: (data: any) => void;
 }
 export class ProductListComponent extends React.Component<StateToProps & DispatchToProps, any> {
     constructor(props: any) {
         super(props);
         this.state = ({
             openModal: false,
-            foodItem: []
+            foodItem: [],
+            pageFood: {
+                pageSize: 9,
+                pageIndex: 1
+            }
         });
         this.openModal = this.openModal.bind(this);
         this.hideModal = this.hideModal.bind(this);
     }
-    // openModal() {
     // @ts-ignore
     openModal(foodItem) {
         const { food } = this.props;
@@ -36,6 +40,8 @@ export class ProductListComponent extends React.Component<StateToProps & Dispatc
     };
 
     componentDidMount() {
+        const {pageFood} = this.state;
+        this.props.setPageListFood(pageFood);
         this.props.getFoodList();
     }
     hideModal() {
@@ -44,14 +50,34 @@ export class ProductListComponent extends React.Component<StateToProps & Dispatc
         });
     }
 
+    forward = (e: any) =>  {
+        e.preventDefault();
+        const {pageFood} = this.state;
+        pageFood.pageIndex = Number(pageFood.pageIndex) + 1;
+        this.props.setPageListFood(pageFood);
+        this.props.getFoodList();
+        this.setState({pageFood});
+    }
+
+    back = (e: any) =>  {
+        e.preventDefault();
+        const {pageFood} = this.state;
+        pageFood.pageIndex = Number(pageFood.pageIndex) - 1;
+        this.props.setPageListFood(pageFood);
+        this.props.getFoodList();
+        this.setState({pageFood});
+    }
+
     public render(): React.ReactNode {
+
+
         // console.log('openModal', this.state.openModal);
         // const { openModal } = this.state;
         // return (
         //     <div className="col-md-7 col-sm-7 text-center">
         //         <div className="panel product-list">
         const { food } = this.props;
-        const {openModal, foodItem} = this.state;
+        const {openModal, foodItem, pageFood} = this.state;
         const resultAll: JSX.Element[] = [];
         const foodItemArr: string[] = [];
         if(food) {
@@ -64,7 +90,7 @@ export class ProductListComponent extends React.Component<StateToProps & Dispatc
                             <img src={item.image}  />
                         </div>
                         <div className="product-footer">
-                {/* //             <p>Chè BlackBall</p> 
+                {/* //             <p>Chè BlackBall</p>
                              <span data-toggle="modal"
                 //                 data-target={openModal}
                 //                 onClick={this.openModal}
@@ -102,7 +128,9 @@ export class ProductListComponent extends React.Component<StateToProps & Dispatc
                         resultAll
                     }
                 </div>
-            
+                <button onClick={(e) => this.back(e)} className="btn btn-secondary" type="button" aria-label="" disabled={(pageFood.pageIndex === 1) ? true : false}>Back</button>
+                <button className="btn btn-secondary" type="button" aria-label="">{pageFood.pageIndex}</button>
+                <button onClick={(e) => this.forward(e)} className="btn btn-secondary" type="button" aria-label="">Forward</button>
               <Modal show={openModal} handleClose={this.hideModal}>
                     <AdditionalForm foodInfo={foodItem} closeModal={this.hideModal}/>
                 </Modal>
@@ -120,6 +148,7 @@ export function mapStateToProps(state: any): StateToProps {
 export function mapDispatchToProps(dispatch: any): DispatchToProps {
     return {
         getFoodList: () => dispatch(GetFoodListAction()),
+        setPageListFood: (data) => dispatch(SetPageListFood(data))
     }
 }
 
